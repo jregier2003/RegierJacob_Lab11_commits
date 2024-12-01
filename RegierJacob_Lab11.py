2c676dfabdafdab6243301b04c0c4c2b0a56a51c
Jacob <jregier8@uwo.ca>
2024-12-01 18:58:56 -0500

    Added the create_tridiagonal_matrix function from Lab 10 for constructing matrices.

--- RegierJacob_Lab11.py
+++ RegierJacob_Lab11.py
@@ -2,3 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def create_tridiagonal_matrix(size, below_diag, diag, above_diag):
+    matrix = np.zeros((size, size))
+    np.fill_diagonal(matrix, diag)
+    np.fill_diagonal(matrix[1:], below_diag)
+    np.fill_diagonal(matrix[:, 1:], above_diag)
+    return matrix
+
