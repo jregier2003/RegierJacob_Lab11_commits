52ace4a8ef10d2c00bfd229f9e49902dd12a1c91
Jacob <jregier8@uwo.ca>
2024-12-01 19:50:49 -0500

    Add plot_wave function to visualize results and tested. Fixed amplification matrix calcluation in advection1d function.

--- RegierJacob_Lab11.py
+++ RegierJacob_Lab11.py
@@ -28,13 +28,18 @@ def advection1d(method, nspace, ntime, tau_rel, params):
     #initial conditions
     sigma, k = 0.2, 35  
     a[:, 0] = initialize_wavepacket(sigma, k, x)
+    print("Initial wavepacket:", a[:, 0])
 
     #calculate A matrix depending on method
     if method == "ftcs":
         A = create_tridiagonal_matrix(nspace, c * tau / (2 * h), 1, -c * tau / (2 * h))
     elif method == "lax":
-        A = create_tridiagonal_matrix(nspace, 0.5 * c * tau / h, 0, -0.5 * c * tau / h)
-        np.fill_diagonal(A, 1)
+        A = np.zeros((nspace, nspace))
+
+        for i in range(nspace):
+            A[i, (i - 1) % nspace] = 0.5 
+            A[i, (i + 1) % nspace] = 0.5 
+        np.fill_diagonal(A, 0)
 
     for n in range(1, ntime):
         a[:, n] = np.dot(A, a[:, n - 1])
@@ -43,3 +48,26 @@ def advection1d(method, nspace, ntime, tau_rel, params):
         print("Warning: FTCS method is unstable for this configuration.")
 
     return a, x, t, A
+
+def plot_wave(a, x, t, plotskip=50, filename="wave_plot.png"):
+    """Visualize wave propagation over time."""
+    fig, ax = plt.subplots()
+    yoffset = a[:, 0].max() - a[:, 0].min()
+    for i in range(len(t) - 1, -1, -plotskip):
+        ax.plot(x, a[:, i] + yoffset * i / plotskip, label=f"t = {t[i]:.3f}")
+    ax.set_xlabel("X Position")
+    ax.set_ylabel("Amplitude [offset]")
+    ax.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Test parameters
+    nspace, ntime = 300, 500
+    tau_rel, L, c = 1, 5, 1
+    params = [L, c]
+
+    a, x, t, A = advection1d("lax", nspace, ntime, tau_rel, params)
+
+    #Visualize results
+    plot_wave(a, x, t)
